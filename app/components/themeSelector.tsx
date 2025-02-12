d4ef0103cbf308ed644addacf9995ca0a0223537
Andcool-Systems <drolll2007@gmail.com>
2025-02-12 21:46:00 +0300

    better theme icon animations

--- app/components/themeSelector.tsx
+++ app/components/themeSelector.tsx
@@ -1,5 +1,5 @@
 import useCookie from '../modules/useCookie';
-import { JSX, useState } from 'react';
+import { JSX, useRef, useState } from 'react';
 import { setTheme } from '../modules/setTheme';
 import styles_about from '@/app/styles/components/about.module.css';
 import { IconMoon, IconSun } from '@tabler/icons-react';
@@ -9,41 +9,38 @@ import langs from '@/app/langs.json';
 
 const raleway = Raleway({ subsets: ['cyrillic', 'latin'] });
 
-export const ThemeSelector = ({ lang_code }: { lang_code: 'ru' | 'en' }) => {
-    const sleep = (time: number) => new Promise((resolve) => setTimeout(resolve, time));
-    const [dark, setDark] = useState<boolean>((useCookie('theme') ?? 'dark') === 'dark');
+const getIcon = (dark: boolean) => {
     const iconProps = {
         width: 20,
-        height: 20,
-        className: styles_about.theme_icon,
-        id: 'theme_icon'
+        height: 20
     };
-    const [icon, setIcon] = useState<JSX.Element>(dark ? <IconMoon {...iconProps} /> : <IconSun {...iconProps} />);
+    return dark ? <IconMoon {...iconProps} /> : <IconSun {...iconProps} />;
+};
 
-    const themeChanged = async (dark: boolean) => {
+export const ThemeSelector = ({ lang_code }: { lang_code: 'ru' | 'en' }) => {
+    const timeoutRef = useRef<NodeJS.Timeout>(null);
+    const [dark, setDark] = useState<boolean>((useCookie('theme') ?? 'dark') === 'dark');
+    const [opacity, setOpacity] = useState<string>('1');
+    const [icon, setIcon] = useState<JSX.Element>(getIcon(dark));
+
+    const themeChanged = (dark: boolean) => {
+        if (timeoutRef.current) clearTimeout(timeoutRef.current);
         const theme_str = dark ? 'dark' : 'light';
-        setDark(dark);
         setCookie('theme', theme_str, { maxAge: 60 * 24 * 365 * 10 });
+        setDark(dark);
         setTheme(theme_str);
-        const hiding_icon = document.getElementById('theme_icon');
-        if (hiding_icon) hiding_icon.style.opacity = '0';
+        setOpacity('0');
 
-        await sleep(300);
-        setIcon(
-            dark ? (
-                <IconMoon {...iconProps} style={{ opacity: 0 }} />
-            ) : (
-                <IconSun {...iconProps} style={{ opacity: 0 }} />
-            )
-        );
-
-        await sleep(15);
-        const showing_icon = document.getElementById('theme_icon');
-        if (showing_icon) showing_icon.style.opacity = '1';
+        timeoutRef.current = setTimeout(() => {
+            setIcon(getIcon(dark));
+            setOpacity('1');
+        }, 300);
     };
     return (
         <button className={styles_about.button} onClick={() => themeChanged(!dark)}>
-            {icon}
+            <div style={{ opacity }} className={styles_about.opacity_icon}>
+                {icon}
+            </div>
             <span className={raleway.className}>{langs[dark ? 'dark_theme' : 'light_theme'][lang_code]}</span>
         </button>
     );
