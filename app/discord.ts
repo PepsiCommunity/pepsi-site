e7c3e9171105997ad66b2e17375cc4d4412256e0
Andcool-Systems <drolll2007@gmail.com>
2025-06-08 00:50:56 +0300

    feat: Added discord error handling

--- app/discord.ts
+++ app/discord.ts
@@ -1,20 +1,26 @@
 const getPepsiMembersCount = async (): Promise<number> => {
-    const response = await fetch(
-        `https://discord.com/api/v10/guilds/${process.env.SERVER_ID}?with_counts=true`,
-        {
-            headers: {
-                authorization: `Bot ${process.env.BOT_TOKEN}`
-            },
-            cache: 'force-cache',
-            next: {
-                revalidate: 300
+    let count = 15;
+    try {
+        const response = await fetch(
+            `https://discord.com/api/v10/guilds/${process.env.SERVER_ID}?with_counts=true`,
+            {
+                headers: {
+                    authorization: `Bot ${process.env.BOT_TOKEN}`
+                },
+                cache: 'force-cache',
+                next: {
+                    revalidate: 300
+                }
             }
-        }
-    );
+        );
 
-    if (!response.ok) throw Error('Cannot fetch discord data');
-    const json = await response.json();
-    return json['approximate_member_count'];
+        if (!response.ok) throw Error('Cannot fetch discord data');
+        const json = await response.json();
+        count = json['approximate_member_count'];
+    } catch {
+        console.error('Error while fetching discord data');
+    }
+    return count;
 };
 
 export default getPepsiMembersCount;
